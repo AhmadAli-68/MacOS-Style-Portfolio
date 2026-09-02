5142ebf87ddfb12876543b281fc5be621e536b3d
Ahmad Ali <ahmad.ali619x@gmail.com>
2026-09-02 19:42:44 +0500

    Implement the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,35 @@
+import dayjs from 'dayjs'
+
+import { navLinks, navIcons } from '@constants'
+
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <div>
+        <img src='/images/logo.svg' alt='logo' />
+        <p className='font-bold'>Ahmad's Portfolio</p>
+
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          {navIcons.map(({ id, img, title }) => (
+            <li key={id}>
+              <img src={img} className='icon' alt={title} />
+            </li>
+          ))}
+        </ul>
+
+        <time>{dayjs().format('ddd MMM D h:mm A')}</time>
+      </div>
+    </nav>
   )
 }
 
